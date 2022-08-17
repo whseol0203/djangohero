0b7c1f8412527c86ed56369146d89ccde1dd1dfc
choeyoha <yoha6865@naver.com>
2022-08-17 18:38:52 +0900

    admin.py 추가

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Auth
 
+# 220817 추가 모델들 = Comment, Profile
+from .models import Comment, Profile
+
 # 220810
 from .models import PostInfo
 
@@ -9,3 +12,7 @@ admin.site.register(Auth)
 
 # 220810
 admin.site.register(PostInfo)
+
+# 220817 추가 모델들 admin 추가
+admin.site.register(Comment)
+admin.site.register(Profile)
